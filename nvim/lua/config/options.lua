b92e8fac0ceb53b733f5fb4372f4d157a8a0cb90
Marius Meschter <git@meschter.me>
2023-05-21 21:09:03 +0200

    move neovim config to seperate repo
    now at https://github.com/onestay/nvim

--- nvim/lua/config/options.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 0
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.smarttab = true
-vim.opt.smartcase = true
-vim.opt.number = true
-vim.opt.relativenumber = true
--- nvim_lazy/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 0
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.smarttab = true
-vim.opt.smartcase = true
